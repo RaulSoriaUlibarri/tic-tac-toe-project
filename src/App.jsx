402f5e53e3e248dc5e3229f9b930ae7e1036b889
Raul <soria.developer@gmail.com>
2026-04-22 18:30:15 -0600

    feature: integrate handleselectsquare logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import { Player } from "./components";
+import { useState } from "react";
+import { Player, GameBoard } from "./components";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((currActivPlayer) => {
+      return currActivPlayer === "X" ? "O" : "X";
+    });
+  }
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player1" symbol="X" />
-          <Player initialName="Player2" symbol="X" />{" "}
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player2"
+            symbol="X"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        GAME BOARD
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
     </main>
   );
