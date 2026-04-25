8c9ca1e83c8fc3c868eae80f17107d86f53ae42c
Raul <soria.developer@gmail.com>
2026-04-24 22:09:35 -0600

    feature(app): integrate winner logic

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,36 +1,7 @@
-import { useState } from "react";
-
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-const GameBoard = ({ onSelectSquare, turns }) => {
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
-  //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  //   function handleSelectSquare(rowIndex, colIndex) {
-  //     setGameBoard((prev) => {
-  //       const updatedBoard = [...prev.map((innerArray) => [...innerArray])];
-  //       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //       return updatedBoard;
-  //     });
-
-  //     onSelectSquare();
-  //   }
-
+const GameBoard = ({ onSelectSquare, board }) => {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => {
+      {board.map((row, rowIndex) => {
         return (
           <li key={rowIndex} id="initial-board">
             <ol>
